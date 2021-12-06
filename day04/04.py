2a12846f0b594935cf364e7a25ed4352b34c28dc
vinicius <vinicius.ishioka@gmail.com>
2021-12-06 00:41:14 -0300

    Day 4 done

--- day04/04.py
+++ day04/04.py
@@ -1,3 +1,28 @@
+def checkwinner(game):
+    for i in range(len(game)):
+        if game[i] == [-1, -1, -1 , -1, -1]:
+            return True
+        column = [line[i] for line in game]
+        if column == [-1, -1, -1, -1, -1]:
+            return True
+
+    return False
+
+def printgame(game):
+    for line in game:
+        for number in line:
+            print(str(number) + ' ', end="")
+        print('\n')
+
+def calcpoints(game, drawing):
+    sum = 0
+    for line in game:
+        for number in line:
+            if number != -1:
+                sum += int(number)
+    print(f'{sum} * {drawing}')
+    return sum*int(drawing)
+
 def part1(input):
     with open(input, 'r') as fp:
         data = fp.read().splitlines()
@@ -7,7 +32,6 @@ def part1(input):
         games = []
         game = []
         for line in data[2:]:
-            print(line)
             if line != '':
                 game.append([n for n in line.split()])
             else:
@@ -15,12 +39,53 @@ def part1(input):
                 game = []
                 
         games.append(game)
+
+        found = False
         
-        for game in games:
-            print(game)
+        for drawing in drawings:
+            if found:
+                break
+            for game in games:
+                for line in game:
+                    if drawing in line:
+                        line[line.index(drawing)] = -1
+                        break
+                if checkwinner(game):
+                    found = True
+                    print(calcpoints(game, drawing))
 
-        #for drawing in drawings:
+def part2(input):
+    with open(input, 'r') as fp:
+        data = fp.read().splitlines()
+        drawings = data[0]
+        drawings = drawings.split(',')
 
+        games = []
+        game = []
+        for line in data[2:]:
+            if line != '':
+                game.append([n for n in line.split()])
+            else:
+                games.append(game)
+                game = []
+                
+        games.append(game)
 
-            
-part1('test')
+        for drawing in drawings:
+            if len(games) == 0:
+                break
+            for game in games[:]:
+                for line in game:
+                    if drawing in line:
+                        line[line.index(drawing)] = -1
+                        break
+
+                if checkwinner(game):
+                    games.remove(game)
+                    if len(games) == 0:
+                        print(calcpoints(game, drawing))
+                
+
+part1('input')
+
+part2('input')
